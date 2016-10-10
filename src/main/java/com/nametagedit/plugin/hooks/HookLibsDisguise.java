3f9eff107278c399b2bc60e30aea7ef6f8203ef9
sgtcaze <sgtcaze@outlook.com>
2016-10-10 10:18:48 -0700

    Attempt to fix LibsDisguises support

--- src/main/java/com/nametagedit/plugin/hooks/HookLibsDisguise.java
+++ src/main/java/com/nametagedit/plugin/hooks/HookLibsDisguise.java
@@ -2,10 +2,11 @@ package com.nametagedit.plugin.hooks;
 
 import com.nametagedit.plugin.NametagEdit;
 import lombok.AllArgsConstructor;
-import me.libraryaddict.disguise.events.UndisguiseEvent;
+import me.libraryaddict.disguise.events.DisguiseEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
 
 @AllArgsConstructor
 public class HookLibsDisguise implements Listener {
@@ -13,9 +14,15 @@ public class HookLibsDisguise implements Listener {
     private NametagEdit plugin;
 
     @EventHandler
-    public void onUndisguiseEvent(UndisguiseEvent event) {
+    public void onDisguiseEvent(final DisguiseEvent event) {
         if (event.getEntity() instanceof Player) {
-            plugin.getHandler().applyTagToPlayer((Player) event.getEntity());
+            plugin.getHandler().getNametagManager().reset(event.getEntity().getName());
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    plugin.getHandler().applyTagToPlayer((Player) event.getEntity());
+                }
+            }.runTaskLater(plugin, 3);
         }
     }
 
