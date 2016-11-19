dd90ebd01c7c97474f671b87ba185d237232e245
sgtcaze <sgtcaze@users.noreply.github.com>
2016-11-18 16:06:15 -0800

    Huge 1.11 Update Lots of Work

--- src/main/java/com/nametagedit/plugin/packets/PacketData.java
+++ src/main/java/com/nametagedit/plugin/packets/PacketData.java
@@ -10,7 +10,8 @@ enum PacketData {
     v1_7("e", "c", "d", "a", "f", "g", "b"),
     v1_8("g", "c", "d", "a", "h", "i", "b"),
     v1_9("h", "c", "d", "a", "i", "j", "b"),
-    v1_10("h", "c", "d", "a", "i", "j", "b");
+    v1_10("h", "c", "d", "a", "i", "j", "b"),
+    v1_11("h", "c", "d", "a", "i", "j", "b");
 
     private String members;
     private String prefix;
