545ca3826ad7ea74893fe83d1e55659e2e9795e3
sgtcaze <sgtcaze@outlook.com>
2017-02-17 10:41:00 -0800

    Add 2 safeguards for Bungee networks and Team name length

--- src/main/java/com/nametagedit/plugin/api/data/FakeTeam.java
+++ src/main/java/com/nametagedit/plugin/api/data/FakeTeam.java
@@ -3,6 +3,7 @@ package com.nametagedit.plugin.api.data;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  * This class represents a Scoreboard Team. It is used
@@ -15,6 +16,9 @@ public class FakeTeam {
     // This represents the number of FakeTeams that have been created.
     // It is used to generate a unique Team name.
     private static int ID = 0;
+    // Because some networks use NametagEdit on multiple servers, we may have clashes
+    // with the same Team names. The UNIQUE_ID ensures there will be no clashing.
+    private static final String UNIQUE_ID = UUID.randomUUID().toString().substring(0, 5);
 
     private final ArrayList<String> members = new ArrayList<>();
     private String name;
@@ -22,7 +26,10 @@ public class FakeTeam {
     private String suffix = "";
 
     public FakeTeam(String prefix, String suffix, String name) {
-        this.name = name == null ? "NTE-" + ++ID : name + ++ID;
+        this.name = name == null ? "NTE-" + UNIQUE_ID + ++ID : name + UNIQUE_ID + ++ID;
+        // It is possible the names of the Team exceeded the length of 16 in the past,
+        // and caused crashes as a result. This is a layer of protection against that.
+        this.name = this.name.length() > 16 ? this.name.substring(0, 16) : name;
         this.prefix = prefix;
         this.suffix = suffix;
     }
