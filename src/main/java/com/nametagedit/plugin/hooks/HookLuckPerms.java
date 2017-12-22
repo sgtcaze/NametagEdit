5189507450c58746fb43561b741af1d9fe78c060
sgtcaze <sgtcaze@outlook.com>
2017-12-22 12:03:30 -0800

    LuckPerms requires Java 8, Correctly implement Event subscriber, should maybe address #210

--- src/main/java/com/nametagedit/plugin/hooks/HookLuckPerms.java
+++ src/main/java/com/nametagedit/plugin/hooks/HookLuckPerms.java
@@ -1,21 +1,27 @@
 package com.nametagedit.plugin.hooks;
 
 import com.nametagedit.plugin.NametagHandler;
-import lombok.AllArgsConstructor;
+import me.lucko.luckperms.LuckPerms;
+import me.lucko.luckperms.api.LuckPermsApi;
 import me.lucko.luckperms.api.User;
+import me.lucko.luckperms.api.event.EventBus;
 import me.lucko.luckperms.api.event.user.track.UserTrackEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-@AllArgsConstructor
 public class HookLuckPerms implements Listener {
 
     private NametagHandler handler;
 
-    @EventHandler
-    public void onUserTrackEvent(UserTrackEvent event) {
+    public HookLuckPerms(NametagHandler handler) {
+        this.handler = handler;
+        LuckPermsApi api = LuckPerms.getApi();
+        EventBus eventBus = api.getEventBus();
+        eventBus.subscribe(UserTrackEvent.class, this::onUserTrackEvent);
+    }
+
+    private void onUserTrackEvent(UserTrackEvent event) {
         User user = event.getUser();
         Player player = Bukkit.getPlayer(user.getUuid());
         if (player != null) {
