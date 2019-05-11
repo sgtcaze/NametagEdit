04a10be0c1cfaaef642aa7882743c5f1ef0de6d7
Cory Gillenkirk <cgillenkirk@gmail.com>
2019-05-11 05:00:35 -0400

    Update PacketData.java

--- src/main/java/com/nametagedit/plugin/packets/PacketData.java
+++ src/main/java/com/nametagedit/plugin/packets/PacketData.java
@@ -15,7 +15,7 @@ enum PacketData {
     v1_10("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e"),
     v1_11("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e"),
     v1_12("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e"),
-    v1_13("h", "c", "d", "a", "i", "j", "b", "g", "f", "e");
+    v1_13("h", "c", "d", "a", "i", "j", "b", "g", "f", "e"),
     v1_14("h", "c", "d", "a", "i", "j", "b", "g", "f", "e");
     
     private String members;
