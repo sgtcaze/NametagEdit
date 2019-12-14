b17e0fbd5ce32a132d8586188be7edde759a597e
HauserGrim <37507220+HauserGrim@users.noreply.github.com>
2019-12-14 23:40:25 +0200

    Guilds hook

---
+++ src/main/java/com/nametagedit/plugin/hooks/HookGuilds.java
@@ -0,0 +1,24 @@
+package com.nametagedit.plugin.hooks;
+
+import com.nametagedit.plugin.NametagHandler;
+import lombok.AllArgsConstructor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import me.glaremasters.guilds.api.events.base.GuildEvent;
+
+@AllArgsConstructor
+public class HookGuilds implements Listener {
+
+    private NametagHandler handler;
+
+    @EventHandler
+    public void onGuildEvent(GuildEvent event) {
+        Player player = Bukkit.getPlayerExact(event.getPlayer().getName());
+        if (player != null) {
+            handler.applyTagToPlayer(player, false);
+        }
+    }
+
+}
