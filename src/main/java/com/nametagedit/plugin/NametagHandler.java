6b6620c078219f685f4a23f80fd772d0bb3e89cd
Cory G <cgillenkirk@gmail.com>
2021-06-11 13:39:59 -0400

    Update NametagHandler.java

--- src/main/java/com/nametagedit/plugin/NametagHandler.java
+++ src/main/java/com/nametagedit/plugin/NametagHandler.java
@@ -5,7 +5,7 @@ import com.nametagedit.plugin.api.data.INametag;
 import com.nametagedit.plugin.api.data.PlayerData;
 import com.nametagedit.plugin.api.events.NametagEvent;
 import com.nametagedit.plugin.api.events.NametagFirstLoadedEvent;
-//import com.nametagedit.plugin.metrics.Metrics;
+import com.nametagedit.plugin.metrics.Metrics;
 import com.nametagedit.plugin.storage.AbstractConfig;
 import com.nametagedit.plugin.storage.database.DatabaseConfig;
 import com.nametagedit.plugin.storage.flatfile.FlatFileConfig;
@@ -336,8 +336,8 @@ public class NametagHandler implements Listener {
         DISABLE_PUSH_ALL_TAGS = config.getBoolean("DisablePush");
 
         if (config.getBoolean("MetricsEnabled")) {
-            //Metrics m = new Metrics(NametagEdit.getPlugin(NametagEdit.class));
-            //m.addCustomChart(new Metrics.SimplePie("using_spigot", () -> PlaceholderAPIPlugin.getServerVersion().isSpigot() ? "yes" : "no"));
+            Metrics m = new Metrics(NametagEdit.getPlugin(NametagEdit.class));
+            m.addCustomChart(new Metrics.SimplePie("using_spigot", () -> PlaceholderAPIPlugin.getServerVersion().isSpigot() ? "yes" : "no"));
         }
 
         clearEmptyTeamTask = createTask("ClearEmptyTeamsInterval", clearEmptyTeamTask, new Runnable() {
