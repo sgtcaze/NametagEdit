8b325acf7a1de868354728785cd42ae1a8c81f51
Aurelien30000 <aurelien.domino@gmail.com>
2021-06-13 18:29:58 +0200

    1.17 update

--- src/main/java/com/nametagedit/plugin/packets/PacketData.java
+++ src/main/java/com/nametagedit/plugin/packets/PacketData.java
@@ -7,19 +7,20 @@ import lombok.Getter;
 @AllArgsConstructor
 enum PacketData {
 
-    v1_7("e", "c", "d", "a", "f", "g", "b", "NA", "NA", "NA"),
+    v1_7("e", "c", "d", "a", "f", "g", "b", "NA", "NA", "NA", "NA"),
     cauldron("field_149317_e", "field_149319_c", "field_149316_d", "field_149320_a",
-            "field_149314_f", "field_149315_g", "field_149318_b", "NA", "NA", "NA"),
-    v1_8("g", "c", "d", "a", "h", "i", "b", "NA", "NA", "e"),
-    v1_9("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e"),
-    v1_10("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e"),
-    v1_11("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e"),
-    v1_12("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e"),
-    v1_13("h", "c", "d", "a", "i", "j", "b", "g", "f", "e"),
-    v1_14("h", "c", "d", "a", "i", "j", "b", "g", "f", "e"),
-    v1_15("h", "c", "d", "a", "i", "j", "b", "g", "f", "e"),
-    v1_16("h", "c", "d", "a", "i", "j", "b", "g", "f", "e");
-    
+            "field_149314_f", "field_149315_g", "field_149318_b", "NA", "NA", "NA", "NA"),
+    v1_8("g", "c", "d", "a", "h", "i", "b", "NA", "NA", "e", "NA"),
+    v1_9("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e", "NA"),
+    v1_10("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e", "NA"),
+    v1_11("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e", "NA"),
+    v1_12("h", "c", "d", "a", "i", "j", "b", "NA", "f", "e", "NA"),
+    v1_13("h", "c", "d", "a", "i", "j", "b", "g", "f", "e", "NA"),
+    v1_14("h", "c", "d", "a", "i", "j", "b", "g", "f", "e", "NA"),
+    v1_15("h", "c", "d", "a", "i", "j", "b", "g", "f", "e", "NA"),
+    v1_16("h", "c", "d", "a", "i", "j", "b", "g", "f", "e", "NA"),
+    v1_17("j", "b", "c", "i", "h", "g", "a", "f", "e", "d", "k");
+
     private String members;
     private String prefix;
     private String suffix;
@@ -30,5 +31,7 @@ enum PacketData {
     private String color;
     private String push;
     private String visibility;
+    //1.17+
+    private String params;
 
 }
