ee55d4e0cc5407ae3b9938ec3eff43019d43bd52
Cory G <cgillenkirk@gmail.com>
2021-12-02 01:43:45 -0500

    I swear I added this the first time.

--- src/main/java/com/nametagedit/plugin/utils/Utils.java
+++ src/main/java/com/nametagedit/plugin/utils/Utils.java
@@ -37,7 +37,7 @@ public class Utils {
 
         switch (VersionChecker.getBukkitVersion()) {
             case v1_13_R1: case v1_14_R1: case v1_14_R2: case v1_15_R1: case v1_16_R1:
-            case v1_16_R2: case v1_16_R3: case v1_17_R1:
+            case v1_16_R2: case v1_16_R3: case v1_17_R1: case v1_18_R1:
                 return limitChars && colored.length() > 256 ? colored.substring(0, 256) : colored;
             default:
                 return limitChars && colored.length() > 16 ? colored.substring(0, 16) : colored;
