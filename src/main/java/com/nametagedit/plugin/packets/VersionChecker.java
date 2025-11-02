069c66a2481576dd486dbb12d925ecf31ee87c30
Aitor Arias Benéitez <80640002+Aitooor@users.noreply.github.com>
2025-11-02 18:06:01 +0100

    feat: Update Spigot version to 1.21.8 and add support for new Bukkit versions

--- src/main/java/com/nametagedit/plugin/packets/VersionChecker.java
+++ src/main/java/com/nametagedit/plugin/packets/VersionChecker.java
@@ -81,6 +81,14 @@ public class VersionChecker {
             bukkitVersion = BukkitVersion.v1_21_3;
         else if (version.contains("(MC: 1.21.4)"))
             bukkitVersion = BukkitVersion.v1_21_4;
+        else if (version.contains("(MC: 1.21.5)"))
+            bukkitVersion = BukkitVersion.v1_21_5;
+        else if (version.contains("(MC: 1.21.6)"))
+            bukkitVersion = BukkitVersion.v1_21_6;
+        else if (version.contains("(MC: 1.21.7)"))
+            bukkitVersion = BukkitVersion.v1_21_7;
+        else if (version.contains("(MC: 1.21.8)"))
+            bukkitVersion = BukkitVersion.v1_21_8;
         else
             bukkitVersion = null;
     }
@@ -96,9 +104,20 @@ public class VersionChecker {
     @Getter
     @AllArgsConstructor
     public enum BukkitVersion {
-        v1_8_R1, v1_8_R2, v1_8_R3, v1_9_R1, v1_9_R2, v1_10_R1, v1_11_R1, v1_12_R1, v1_13_R1, v1_13_R2, v1_14_R1, v1_14_R2, v1_15_R1, v1_16_R1, v1_16_R2, v1_16_R3, v1_17_R1, v1_18_R1, v1_19_R1, v1_19_R2, v1_19_R3, v1_20_R1, v1_20_R2, v1_20_R3,
-        v1_20_5(PacketData.v1_20_5), v1_20_6(PacketData.v1_20_5),
-        v1_21(PacketData.v1_21), v1_21_1(PacketData.v1_21), v1_21_2(PacketData.v1_21), v1_21_3(PacketData.v1_21), v1_21_4(PacketData.v1_21);
+        v1_8_R1, v1_8_R2, v1_8_R3,
+        v1_9_R1, v1_9_R2,
+        v1_10_R1,
+        v1_11_R1,
+        v1_12_R1,
+        v1_13_R1, v1_13_R2,
+        v1_14_R1, v1_14_R2,
+        v1_15_R1,
+        v1_16_R1, v1_16_R2, v1_16_R3,
+        v1_17_R1,
+        v1_18_R1,
+        v1_19_R1, v1_19_R2, v1_19_R3,
+        v1_20_R1, v1_20_R2, v1_20_R3, v1_20_5(PacketData.v1_20_5), v1_20_6(PacketData.v1_20_5),
+        v1_21(PacketData.v1_21), v1_21_1(PacketData.v1_21), v1_21_2(PacketData.v1_21), v1_21_3(PacketData.v1_21), v1_21_4(PacketData.v1_21), v1_21_5(PacketData.v1_21), v1_21_6(PacketData.v1_21), v1_21_7(PacketData.v1_21), v1_21_8(PacketData.v1_21);
 
         private final PacketData packetData;
 
